a15210cf84640dee383972d850cc8d6f3d38bc3d
Fahad Hasan <66886981+hasan-fahad@users.noreply.github.com>
2023-06-09 11:51:11 +0600

    reviews add

--- src/Home/StudentsReview/StudentsReview.jsx
+++ src/Home/StudentsReview/StudentsReview.jsx
@@ -5,20 +5,20 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import 'swiper/css/navigation';
+import { Rating } from "@smastrom/react-rating";
+import '@smastrom/react-rating/style.css'
 
 
 
 import { Autoplay, Pagination, Navigation } from 'swiper';
-import { useRef } from "react";
+
+import useReview from "../../Hooks/useReview";
+
 
 
 const StudentsReview = () => {
-    const progressCircle = useRef(null);
-    const progressContent = useRef(null);
-    const onAutoplayTimeLeft = (s, time, progress) => {
-      progressCircle.current.style.setProperty('--progress', 1 - progress);
-      progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
-    };
+
+    const [review] = useReview([]);
 
     return (
         <div>
@@ -40,24 +40,26 @@ const StudentsReview = () => {
         }}
         navigation={true}
         modules={[Autoplay, Pagination, Navigation]}
-        onAutoplayTimeLeft={onAutoplayTimeLeft}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-        <div className="autoplay-progress" slot="container-end">
-          <svg viewBox="0 0 48 48" ref={progressCircle}>
-            <circle cx="2" cy="2" r="2"></circle>
-          </svg>
-          <span ref={progressContent}></span>
-        </div>
+        {
+                review.map(review => <SwiperSlide
+                key={review._id}
+                
+                >
+                    <div className="mx-24 flex flex-col items-center">
+                                                <Rating className="flex"
+                                style={{ maxWidth: 180 }}
+                                value={review.rating}
+                                readOnly
+                                />
+                        <p className="py-8">{review.details}</p>
+                        <h1 className=" font-semibold text-2xl text-red-600 mb-8">{review.name}</h1>
+                    </div>
+                    
+                     </SwiperSlide>)
+            }
+        
       </Swiper>
         </div>
     );
