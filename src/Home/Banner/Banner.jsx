75dc7a2b8a54231bf5a45849aaa38740092f87d5
Fahad Hasan <66886981+hasan-fahad@users.noreply.github.com>
2023-06-11 18:49:02 +0600

    banner updated

--- src/Home/Banner/Banner.jsx
+++ src/Home/Banner/Banner.jsx
@@ -7,23 +7,25 @@ import img3 from '../../assets/Images/Banner/Banner-3.jpg'
 const Banner = () => {
     return (
         <div>
-             <Carousel className="text-center">
-                <div>
-                    <img src={img1} alt="" />
-                    
-                    
-                </div>
-                <div>
-                    <img src={img2} />
-                    
-                    
-                </div>
-                <div>
-                    <img src={img3} />
-                    
-                </div>
-                
-            </Carousel>
+
+<div className="carousel w-full">
+  <div id="item1" className="carousel-item w-full">
+    <img src={img1} className="w-full" />
+  </div> 
+  <div id="item2" className="carousel-item w-full">
+    <img src={img2} className="w-full" />
+  </div> 
+  <div id="item3" className="carousel-item w-full">
+    <img src={img3} className="w-full" />
+  </div>
+</div> 
+<div className="flex justify-center w-full py-2 gap-2">
+  <a href="#item1" className="btn btn-xs">1</a> 
+  <a href="#item2" className="btn btn-xs">2</a> 
+  <a href="#item3" className="btn btn-xs">3</a> 
+  
+</div>
+             
         </div>
     );
 };
